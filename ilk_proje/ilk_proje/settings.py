e62ff55fe5f8a0ead76c905ebfc5966fafb18889
skose241 <skose241@posta.pau.edu.tr>
2026-09-21 13:53:07 +0300

    PostgreSQL bağlantısnı tamamla

--- ilk_proje/ilk_proje/settings.py
+++ ilk_proje/ilk_proje/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ilk_proje.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'ilk_proje_db',
+        'USER':'postgres',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
